150b6ecb37e707dbc01003f513171afaf702b0bf
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-04 14:58:47 +0900

    05예제 오버로딩 실습

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+#include<iostream>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,42 @@ namespace overridemethos
 {
     class Program
     {
+        int Power(int input)
+        {
+            return input * input;
+        }
+        int Power(int input, int count)
+        {
+            int a = 1;
+            for(int i=1; i<=count; i++)
+            {
+                a = input * a;
+            }
+            return a;
+        }
+
+        int SumAll(int end)
+        {
+            int total = 0;
+            for(int i = 0; i <= end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+        int SumAll(int end, int start)
+        {
+            int total = 0;
+            for (int i = start; i <= end; i++)
+            {
+                total += i;
+            }
+            return total;
+        }
+
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
